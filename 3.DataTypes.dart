6a3103634d67f4f31770fc14f03b1bd46735ead5
alriansyah <alriansyah1218@gmail.com>
2023-08-22 16:47:49 +0700

    Data Types

---
+++ 3.DataTypes.dart
@@ -0,0 +1,17 @@
+import 'dart:io';
+
+void main() {
+  String greetings = 'Hello Dart!';
+  int myAge = 20;
+
+  /*
+  - int	              Integer (bilangan bulat)	            5, -7, 0
+  - double	          Bilangan desimal	                    3.14, 18.0, -12.12
+  - num	              Bilangan bulat dan bilangan desimal	  5, 3.14, -99.00
+  - bool	            Boolean	                              true, false
+  - String	          Teks yang terdiri dari 0 atau beberapa karakter	    ‘Dicoding’, ‘Y’, ‘’
+  - List	            Daftar nilai	                        [1, 2, 3], [‘a’, ‘b’, ‘c’]
+  - Map	              Pasangan key-value	                  {“x”: 4, “y”: 10}
+  - dynamic	          Tipe apa pun	
+  */
+}
